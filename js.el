0cf7096f71edd5f0ff9c4845dfcb1ffd79f61f25
Mike Dalton <michaelcdalton@gmail.com>
2016-11-18 10:27:49 -0500

    Adding commands to print console.log for JavaScript files

--- js.el
+++ js.el
@@ -1,1 +1,9 @@
 (setq js-indent-level 2)
+
+(defun console-log-variable (string)
+  (interactive "sVariable for console.log(\"VARIABLE: \" VARIABLE): ")
+  (insert "console.log(\"" string ": \" + " string ");"))
+
+(defun console-log-json-stringify (string)
+  (interactive "sObject for console.log(JSON.stringify(OBJECT)): ")
+  (insert "console.log(JSON.stringify(" string "));"))
